833b051180857fb557ed754bc27731a5bc4622ec
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2025-12-31 20:28:12 +0200

    init shadcn; add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>hello next</h1>
+      <Button size={"lg"}>Click me</Button>
     </div>
   );
 }
