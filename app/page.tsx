d47ff4d101fc858a4427fd7ad5ec624a81116562
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2026-01-01 13:32:08 +0200

    add shadcn badge; refactor hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1>hello next</h1>
-      <Button size={"lg"}>Click me</Button>
+      <Badge>Join thousands of creators sharing their work</Badge>
+      <h1>Share What You&apos;ve Built, Discover What&apos;s Launching</h1>
+      <p>
+        A community platform for creators to showcase their apps, AI tools, SaaS
+        products, and creative projects. Authentic launches, real builders,
+        genuine feedback.
+      </p>
+      <Button>Share Your Project</Button>
+      <Button>Explore Porjects</Button>
     </div>
   );
 }
