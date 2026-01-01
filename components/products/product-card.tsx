73cca20fdd6d71426d6ae4e97f3c110a3b45836e
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2026-01-01 20:08:20 +0200

    add product card details & styles

--- components/products/product-card.tsx
+++ components/products/product-card.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import { Badge } from "../ui/badge";
+import { StarIcon } from "lucide-react";
+
 interface Product {
   id: number;
   name: string;
@@ -8,5 +19,37 @@ interface Product {
 }
 
 export default function ProductCard({ product }: { product: Product }) {
-  return <div>{product.name}</div>;
+  return (
+    <Link href={`/products/${product.id}`}>
+      <Card className="group card-hover hover:bg-primary-foreground/10 border-solid border-gray-400 min-h-180px">
+        <CardHeader className="flex-1">
+          <div className="flex items-start gap-4">
+            <div className="flex-1 min-w-0">
+              <div className="flex items-center gap-2">
+                <CardTitle className="text-lg group-hover:text-primary transition-color">
+                  {product.name}
+                </CardTitle>
+                {product.isFeatured && (
+                  <Badge className="gap-1 bg-primary text-primary-foreground">
+                    <StarIcon className="size-3 fill-current" /> Featured
+                  </Badge>
+                )}
+              </div>
+              <CardDescription>{product.description}</CardDescription>
+            </div>
+            {/* Voting buttons */}
+          </div>
+        </CardHeader>
+        <CardFooter>
+          <div className="flex items-center gap-2">
+            {product.tags.map((tag) => (
+              <Badge variant="secondary" key={tag}>
+                {tag}
+              </Badge>
+            ))}
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
 }
