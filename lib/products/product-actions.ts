d7f830ff2289567a9a3a91ccca097109d3e2611c
Boyan Kachamachkov <131705950+kachamachkov@users.noreply.github.com>
2026-01-03 10:54:58 +0200

    post new product from form to db

--- lib/products/product-actions.ts
+++ lib/products/product-actions.ts
@@ -1,5 +1,11 @@
 "use server";
 
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { productSchema } from "./product-validation";
+import { db } from "@/db";
+import { products } from "@/db/schema";
+import z from "zod";
+
 type FormState = {
   sucess: boolean;
   errors?: Record<string, string[]>;
@@ -12,9 +18,68 @@ export const addProductAction = async (
 ) => {
   console.log(formData);
 
-  return {
-    success: true,
-    errors: {},
-    message: "Product added successfully",
-  };
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return {
+        success: false,
+        message: "You must be signed in to submit a product.",
+      };
+    }
+
+    const user = await currentUser();
+    const userEmail = user?.primaryEmailAddress?.emailAddress || "anonymous";
+
+    const rawFormData = Object.fromEntries(formData.entries());
+    const validatedData = productSchema.safeParse(rawFormData);
+
+    if (!validatedData.success) {
+      console.log(validatedData.error.flatten().fieldErrors);
+      return {
+        success: false,
+        errors: validatedData.error.flatten().fieldErrors,
+        message: "Invalid data",
+      };
+    }
+
+    const { name, slug, tagline, description, websiteUrl, tags } =
+      validatedData.data;
+
+    const tagsArray = tags ? tags.filter((tag) => typeof tag === "string") : [];
+
+    await db.insert(products).values({
+      name,
+      slug,
+      tagline,
+      description,
+      websiteUrl,
+      tags: tagsArray,
+      status: "pending",
+      submittedBy: userEmail,
+      userId,
+    });
+
+    return {
+      success: true,
+      message: "Product submitted successfully! It will be reviewed shortly.",
+      errors: undefined,
+    };
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof z.ZodError) {
+      return {
+        success: false,
+        errors: error.flatten().fieldErrors,
+        message: "Validation failed. Please check the form.",
+      };
+    }
+
+    return {
+      success: false,
+      errors: undefined,
+      message: "Failed to submit product",
+    };
+  }
 };
